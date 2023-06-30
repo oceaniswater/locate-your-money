e30a5872251184d694ed5bd9f379c2a38aa398b0
oceaniswater <markflyingbear@gmail.com>
2023-06-30 10:08:25 +0100

    Initial Commit

---
+++ locate your money/locate_your_moneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  locate_your_moneyApp.swift
+//  locate your money
+//
+//  Created by Mark Golubev on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct locate_your_moneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
